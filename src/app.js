724eb6c2ae67e5ec2e5326f7792817466b166bfa
Flavio Ribeiro <flavioribeiro.tech@gmail.com>
2020-06-22 21:55:11 -0300

    chore: change to airbnb eslint style guide

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
-import express, { json, urlencoded } from 'express'
-import logger from 'morgan'
-import router from './router'
+import express, { json, urlencoded } from 'express';
+import logger from 'morgan';
+import router from './router';
 
-const app = express()
+const app = express();
 
-app.use(logger('dev'))
-app.use(json())
-app.use(urlencoded({ extended: false }))
+app.use(logger('dev'));
+app.use(json());
+app.use(urlencoded({ extended: false }));
 
 /**
  * OAuth authentication routes.
  */
-app.use(router)
+app.use(router);
 
-app.set('port', 3001)
+app.set('port', 3001);
 
-export default app
+export default app;
--- src/server.js
+++ src/server.js
@@ -1,54 +1,54 @@
-import errorHandler from 'errorhandler'
+import errorHandler from 'errorhandler';
 
-import app from './app'
+import app from './app';
 
-const debug = require('debug')('express-es6:server')
+const debug = require('debug')('express-es6:server');
 
 /**
  * Error Handler. Provides full stack - remove for production
  */
-app.use(errorHandler())
+app.use(errorHandler());
 
 const onError = (error) => {
   if (error.syscall !== 'listen') {
-    throw error
+    throw error;
   }
 
   const bind = typeof port === 'string'
     ? `Pipe ${app.get('port')}`
-    : `Port ${app.get('port')}`
+    : `Port ${app.get('port')}`;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      debug(`${bind} requires elevated privileges`) // eslint-disable-line
-      process.exit(1)
-      break
+      debug(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
     case 'EADDRINUSE':
-      debug(`${bind} is already in use`) // eslint-disable-line
-      process.exit(1)
-      break
+      debug(`${bind} is already in use`);
+      process.exit(1);
+      break;
     default:
-      throw error
+      throw error;
   }
-}
+};
 
 /**
  * Start Express server.
  */
-const server = app.listen(app.get('port'))
+const server = app.listen(app.get('port'));
 
 const onListening = () => {
-  const addr = server.address()
+  const addr = server.address();
   const bind = typeof addr === 'string'
     ? `pipe ${addr}`
-    : `port ${addr.port}`
-  const env = app.get('env')
+    : `port ${addr.port}`;
+  const env = app.get('env');
 
-  debug(`Running at ${bind} in ${env} mode`)
-}
+  debug(`Running at ${bind} in ${env} mode`);
+};
 
-server.on('error', onError)
-server.on('listening', onListening)
+server.on('error', onError);
+server.on('listening', onListening);
 
-export default server
+export default server;
